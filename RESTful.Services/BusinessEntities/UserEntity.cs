57cd5f68ed35f24000570c5c5aac8e38daab7a47
Maurice Muoneke <obibiman@yahoo.com>
2016-04-24 19:32:36 -0500

    Commit after Day 01 code

---
+++ RESTful.Services/BusinessEntities/UserEntity.cs
@@ -0,0 +1,10 @@
+namespace BusinessEntities
+{
+    public class UserEntity
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RESTful.Services/BusinessServices/IProductServices.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BusinessEntities;
+
+namespace BusinessServices
+{
+    public interface IProductServices
+    {
+        ProductEntity GetProductById(int productId);
+        IEnumerable<ProductEntity> GetAllProducts();
+        int CreateProduct(ProductEntity productEntity);
+        bool UpdateProduct(int productId, ProductEntity productEntity);
+        bool DeleteProduct(int productId);
+    }
+}
